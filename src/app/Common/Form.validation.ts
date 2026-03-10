c60eb318346deeb7391587d3b564401a0a409085
SovannaraHong <rapewpew372@gmail.com>
2026-03-10 20:54:47 +0700

    reactive dinamic

--- src/app/Common/Form.validation.ts
+++ src/app/Common/Form.validation.ts
@@ -1,6 +1,24 @@
-import { AbstractControl } from '@angular/forms';
+import { ValueEqualityFn } from '@angular/core';
+import { AbstractControl, ValidatorFn } from '@angular/forms';
 
-export function usernameValidate(control: AbstractControl): { [key: string]: any } | null {
-  const forbidden = /admin/.test(control.value);
-  return forbidden ? { forbiddenName: control.value } : null;
+// export function usernameValidate(control: AbstractControl): { [key: string]: any } | null {
+//   const forbidden = /admin/.test(control.value);
+//   return forbidden ? { forbiddenName: control.value } : null;
+// }
+export function usernameValidate(pattern: RegExp): ValidatorFn {
+  const validator: ValidatorFn = (control: AbstractControl): { [key: string]: any } | null => {
+    if (!control.value) return null;
+    const forbidden = pattern.test(control.value);
+    return forbidden ? { forbiddenName: control.value } : null;
+  };
+  return validator;
+}
+
+export function forbiddenWordValidate(pattern: string[]): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const value = control.value?.toLowerCase();
+    if (!value) return null;
+    const foundWord = pattern.find((word) => value.includes(word.toLowerCase()));
+    return foundWord ? { foundingWord: { word: foundWord } } : null;
+  };
 }
--- src/app/Form/reactive-form/reactive-form.ts
+++ src/app/Form/reactive-form/reactive-form.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { usernameValidate } from '../../Common/Form.validation';
+import { forbiddenWordValidate, usernameValidate } from '../../Common/Form.validation';
 
 @Component({
   selector: 'app-reactive-form',
@@ -12,9 +12,15 @@ import { usernameValidate } from '../../Common/Form.validation';
 export class ReactiveForm implements OnInit {
   constructor(private fb: FormBuilder) {}
   registrationForm!: FormGroup;
+
   ngOnInit(): void {
+    const validateUsername = ['admin', 'password', 'email', 'root'];
     this.registrationForm = this.fb.group({
-      username: ['', [Validators.required, Validators.minLength(4), usernameValidate]], //first parameter is field name and second parameter is condition
+      // username: ['', [Validators.required, Validators.minLength(4), usernameValidate(/admin/)]], //first parameter is field name and second parameter is condition
+      username: [
+        '',
+        [Validators.required, Validators.minLength(4), forbiddenWordValidate(validateUsername)],
+      ],
       password: [''],
       confirmPassword: [''],
       address: this.fb.group({
